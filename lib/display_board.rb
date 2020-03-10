178ebdecb9521866e3ead43e15a1f0a59ad40342
mystical-system-5659 <jennifer19hernandez96@gmail.com>
2020-03-10 02:42:48 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,3 +2,8 @@ board= " ", " ", " ", " ", " ", " ", " ", " ", " "
 def display_board
   puts "display_board(board)"
 end
+
+board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+def display_board
+  puts "display_board(board)"
+end
